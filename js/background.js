8f34fa87ccc54d8eac0733bd2f065abadbb20785
Noah Benham <noah.benham@cerner.com>
2018-04-13 09:48:17 -0500

    Simplify opening page on install, lint script

--- js/background.js
+++ js/background.js
@@ -1,11 +1,3 @@
-function installNotice() {
-  chrome.storage.sync.get(['installTime'], (result) => {
-    if (result.installTime) return;
-
-    const now = new Date().getTime();
-    chrome.storage.sync.set({ installTime: now });
-    chrome.tabs.create({ url: 'options.html' });
-  });
-}
-
-installNotice();
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.create({ url: 'options.html' });
+});

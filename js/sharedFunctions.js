270733a929da264c11d0b269057dca3e3fb88e6b
Noah Benham <noah.benham@cerner.com>
2018-04-13 09:42:35 -0500

    Cleanup all lint

--- js/sharedFunctions.js
+++ js/sharedFunctions.js
@@ -1,4 +1,4 @@
-function getDefaultOptions() {
+function getDefaultOptions() { // eslint-disable-line no-unused-vars
   return {
     jiraPath: 'https://jira2.cerner.com/',
     regex: '(CONNECT)-[\\d]{1,6}',

f7be7407701ef90b2613be3ff53ff08b815eecd6
Noah Benham <noah.benham@cerner.com>
2018-04-12 21:55:15 -0500

    Improve ajax fetching with promise, page parsing

--- convert_keys.js
+++ convert_keys.js
@@ -1,95 +1,59 @@
-let jiraPath;
-let regex;
+function fetchJiraStatus(key) {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching JIRA data for ${key}`);
+    const xhr = new XMLHttpRequest();
 
-function searchForKeyNames() {
-  function createLinkFromNode(node) {
-    const txt = node.textContent;
-    let mVar;
-    let span;
-    let pVar = 0;
-
-    if (txt.trim().length === 0) return;
-
-    const jiraTagExpression = new RegExp(`(${regex})`, 'g');
-
-    while ((mVar = jiraTagExpression.exec(txt)) !== null) {
-      if (!span) {
-        // Create a new span for the replaced text and newly created href
-        span = document.createElement('span');
-      }
-
-      // Get the link without trailing dots
-      const link = mVar[0].replace(/\.*$/, '');
-
-      // Put in text up to the link
-      span.appendChild(document.createTextNode(txt.substring(pVar, mVar.index)));
-
-      // Create a link and put it in the span
-      const a = document.createElement('a');
-      a.className = 'linkclass';
-      a.appendChild(document.createTextNode(link));
-      a.setAttribute('href', jiraPath + link);
-      a.style.textDecoration = 'underline';
-
-      span.appendChild(a);
-      // track insertion point
-      pVar = mVar.index + mVar[0].length;
-    }
-    if (span) {
-      // Take the text after the last link
-      span.appendChild(document.createTextNode(txt.substring(pVar, txt.length)));
-
-      // Replace the original text with the new span
-      try {
-        node.parentNode.replaceChild(span, node);
-      } catch (e) {
-        console.error(e);
-        console.log(node);
-      }
-    }
-  }
-
-  if (document.contentType !== 'text/xml' && document.contentType !== 'application/xml') {
-    let node,
-      allLinks = findTextNodes();
-    for (let i = 0; i < allLinks.length; i++) {
-      node = allLinks[i];
-      createLinkFromNode(node);
-    }
-  }
+    xhr.open('GET', `https://jira2.cerner.com/rest/api/2/issue/${key}?fields=status`, true);
+    xhr.onload = () => resolve(JSON.parse(xhr.responseText).fields.status.name);
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
 }
 
-const observer = new MutationObserver(() => {
-  observer.stop();
-  searchForKeyNames();
-  observer.start();
-});
-observer.start = () => {
-  observer.observe(document.body, {
-    attributes: false,
-    characterData: false,
-    childList: true,
-    subtree: true,
-  });
-};
-observer.stop = () => observer.disconnect;
+function getDefaultOptions() {
+  return {
+    jira_path: 'https://jira2.cerner.com/browse/',
+    regex: '(CONNECT)-[\\d]{1,6}',
+  };
+}
 
-function findTextNodes(root) {
-  root = root || document.body;
+function getSavedOptions() {
+  if (localStorage.options && localStorage.options.length) {
+    return JSON.parse(localStorage.options);
+  }
 
-  const textNodes = [];
+  return null;
+}
 
-  const ignoreTags = /^(?:a|noscript|option|script|style|textarea)$/i;
-  (function findTextNodes(node) {
-    node = node.firstChild;
-    while (node) {
-      if (node.nodeType === 3) {
-        textNodes.push(node);
-      } else if (!ignoreTags.test(node.nodeName)) {
-        findTextNodes(node);
+function replaceText(index, element, status) {
+  console.log(index);
+  console.log(element);
+  console.log(status);
+}
+
+const options = getSavedOptions() || getDefaultOptions();
+
+
+const match = new RegExp(options.regex, 'gi');
+const elements = document.getElementsByTagName('*');
+
+for (let i = 0; i < elements.length; i += 1) {
+  const element = elements[i];
+
+  for (let j = 0; j < element.childNodes.length; j += 1) {
+    const node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      const text = node.nodeValue;
+
+      const matches = match.exec(text);
+      if (matches) {
+        fetchJiraStatus(matches[0]).then((status) => {
+          replaceText(matches.index, element, status);
+        }).catch((err) => {
+          console.error(err);
+        });
       }
-      node = node.nextSibling;
     }
-  }(root));
-  return textNodes;
+  }
 }

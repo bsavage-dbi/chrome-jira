85116d071a509737c9db7e89b8272f7827e0ed4a
Noah Benham <noah.benham@cerner.com>
2018-04-16 18:38:25 -0500

    Open Changelog on update

--- js/background.js
+++ js/background.js
@@ -1,5 +1,7 @@
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     chrome.tabs.create({ url: 'html/options.html' });
+  } else if (details.reason === 'update') {
+    chrome.tabs.create('https://github.cerner.com/NB042447/chrome-jira/blob/master/CHANGELOG.md');
   }
 });

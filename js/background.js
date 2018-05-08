ab51dfa765714e5fb4347930bb1844dc0f22c4ff
Noah Benham <noah.benham@cerner.com>
2018-05-07 20:16:37 -0500

    Remove options conversion upon upgrade

--- js/background.js
+++ js/background.js
@@ -1,23 +1,5 @@
-function setNewDefaults(options) {
-  if (!options.documentQueries) {
-    chrome.storage.sync.set({ options: { ...options, documentQueries: 'p.commit-title, span.branch-details' } });
-  }
-}
-
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     chrome.tabs.create({ url: 'html/options.html' });
-  } else if (details.reason === 'update') {
-    // TODO remove once everyone is upgraded
-    chrome.storage.sync.get(['options'], (result) => {
-      try {
-        const convertedOptions = JSON.parse(result.options);
-        chrome.storage.sync.set({ options: convertedOptions }, () =>
-          setNewDefaults(convertedOptions));
-      } catch (err) {
-        console.log('Options already converted, thanks for upgrading.');
-        setNewDefaults(result.options);
-      }
-    });
   }
 });
